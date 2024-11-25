7301c728dfec6be5d9211f7d315f9a40ac9bd4ab
the-lizard-king <93225050+the-lizard-king@users.noreply.github.com>
2024-11-25 14:09:45 -0800

    Add files via upload

--- scripts/the-deep-end.js
+++ scripts/the-deep-end.js
@@ -10,13 +10,19 @@ function creatureTalk() {
             modfiyContent("message", "My apologies if I frightend you")
             break;
         case "2":
-            modfiyContent("message", "I don't get many visitors down hear and those I do get tend to not last long")
+            modfiyContent("message", "I don't get many visitors down here and those I do get tend to not last long")
             break;
         case "3":
             modfiyContent("message", "As you can see you have entered the <p id=\"m2\">\"The Deep End\"<\p> ")
             break;
         case "4":
             modfiyContent("message", "And it is in your best interest that you leave as soon as you can")
+            break;
+        case "5":
+            modfiyContent("message", "Why?")
+            break;
+        case "6":
+            modfiyContent("message", "Perhaps I should elaborate on the nature of this place")
             break;
     }
     talkNum++;
